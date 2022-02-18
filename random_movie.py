91b100e097a9fb625bbdd5348c1fffbbfb746342
Leo2660 <56320744+Leo2660@users.noreply.github.com>
2022-02-18 13:23:21 +0100

    add user rating and metascore

--- random_movie.py
+++ random_movie.py
@@ -5,6 +5,7 @@ from os import system, name
 from moviepy.editor import VideoFileClip
 import glob
 import PTN
+import imdb
 
 #clear console function
 def clear():
@@ -20,6 +21,8 @@ def clear():
 root = Tk() #pointing root to Tk() to use it as Tk() in program
 root.withdraw() #hides small tkinter window
 root.attributes('-topmost', True) # Opened windows will be active. above all windows despite of selection
+ia = imdb.IMDb() #create imdb istance
+
 
 clear()
 print("Selezionare la cartella che contiene i film")
@@ -32,8 +35,18 @@ movies_mkv = glob.glob(directory_path + "/**/*.mkv", recursive = True) #trova tu
 movies = movies_mp4 + movies_mkv
 random_movie_path = random.choice(movies)
 movie_name = os.path.basename(os.path.normpath(random_movie_path)) #tiene solo la parte finale del path
-movie_parsed = PTN.parse(movie_name)
+movie_parsed = PTN.parse(movie_name) #returns dict
 duration = VideoFileClip(random_movie_path).duration #ottiene la durata del film
+
+search = ia.search_movie(movie_parsed['title']) #uses title field from parsed movie name
+movie_id = search[0].movieID #gets movie id
+movie_from_id = ia.get_movie(movie_id) #gets movie data using the movie id
+user_rating = movie_from_id.data['rating']
+critics_data = ia.get_movie_critic_reviews(movie_id) #gets critics data, returned value is a dict
+metascore = critics_data['data']['metascore']
+
 print("Il tuo film per stasera è: " + movie_parsed['title'])
 print("Durata: " + time.strftime('%H:%M:%S', time.gmtime(duration)) )
+print("User rating: " + str(user_rating))
+print("Metascore: " + str(metascore))
 
